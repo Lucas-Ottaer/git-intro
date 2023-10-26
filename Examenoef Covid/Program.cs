e2d980516dd7f5674aba3e4461007a00b557dbd5
Lucas-Ottaer <145749636+Lucas-Ottaer@users.noreply.github.com>
2023-10-26 09:44:26 +0200

    Add files via upload

---
+++ Examenoef Covid/Program.cs
@@ -0,0 +1,49 @@
+﻿
+string[] menu = new string[] { "stoppen", "maandtotaal opvragen", "Jaartotaal opvragen" };
+string[] submenu = new string[] { "Aantal besmettingen", "Aantal sterfgevallen" };
+
+
+
+Printmenu("Menu:", menu);
+
+
+
+
+
+
+void DrukOpEnter()
+{
+
+}
+
+
+
+int LeesGetal(int min, int max)
+{
+
+    return 0;
+}
+
+
+int BehandelBestand(int subkeuze, int maand)
+{
+    return 0;
+}
+
+
+
+void DrukResultaat(int subkeuze, int waarde)
+{
+
+
+}
+
+
+void Printmenu(string soort, string[] menu)
+{
+    Console.WriteLine(soort);
+    for (int i = 0; i < menu.Length; i++)
+    {
+        Console.WriteLine($"{i}: {menu[i]}");
+    }
+}
